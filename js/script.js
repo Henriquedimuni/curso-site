7321b7c09448e4ac369b13a9c40c7c0daa837d06
Henrique Muniz <henriquedimuni@gmail.com>
2019-11-20 15:09:52 -0300

    Refatorando scrollsuave.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import navegacaoLista from './modules/navegacaoPorTab.js';
 import accordionList from "./modules/accordionList.js";
-import scrollSuave from "./modules/scrollSuave.js";
+import ScrollSuave from "./modules/scrollSuave.js";
 import initAnimacaoScroll from "./modules/scrollAnime.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -10,7 +10,9 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
-scrollSuave();
+const scrollsuave = new ScrollSuave('[data-menu="menu"] [href^="#"]');
+scrollsuave.init();
+
 initAnimacaoScroll();
 accordionList();
 navegacaoLista();
