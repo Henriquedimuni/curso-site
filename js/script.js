15b6185fa3c85be7c6d65f13fe7e35580202cdcc
Henrique Muniz <henriquedimuni@gmail.com>
2019-11-20 18:32:58 -0300

    posicao scroll alterada

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import navegacaoLista from './modules/navegacaoPorTab.js';
 import accordionList from "./modules/accordionList.js";
 import ScrollSuave from "./modules/scrollSuave.js";
-import initAnimacaoScroll from "./modules/scrollAnime.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -9,11 +8,11 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/scrollAnime.js";
 
 const scrollsuave = new ScrollSuave('[data-menu="menu"] [href^="#"]');
 scrollsuave.init();
 
-initAnimacaoScroll();
 accordionList();
 navegacaoLista();
 initModal();
@@ -23,4 +22,5 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
 
